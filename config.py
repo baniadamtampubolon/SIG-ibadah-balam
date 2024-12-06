ebcac15fe116e19867923b540d49db2fa6adb120
baniadamtampubolon <baniadam.tampubolon@gmail.com>
2024-12-06 11:58:54 +0700

    add sig tubes

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL', 'postgresql://postgres:adam31@localhost:5432/tubes'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
